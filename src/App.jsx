d567e6f0a365b351d5e156085c98301bbd9b99c6
Santiago Ramirez Niño <A01665906@tec.mx>
2025-02-01 03:48:12 -0600

    Cripto list created. Axios added. Style sheet created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
-
+import { useState } from "react"
+import { useEffect } from "react"
+import axios from "axios"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const API_URL = import.meta.env.VITE_API_URL  //peticion tipo GET
+
+  const [criptos, setCriptos] = useState()  // para actualizar estados
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)   //o puede ser fetch, requiere json
+      //.then((resp) => resp.json())  //la respuesta recibida la convertira a un json
+      .then((data) => {
+        setCriptos(data.data.data)
+      })
+      .catch(() => {console.error("La petición falló")})
+  } , [])
+
+  if (!criptos) return <span> Cargando ... </span>
+
   return(
-    <h1>HOLA MUNDO</h1>
+    <>
+      <h1>Lista de Criptomonedas</h1>
+      <ol>
+        {criptos.map(({id, name, priceUsd}) => (
+          <li key={id}> Nombre: {name} Precio: {priceUsd} </li>
+        ))}
+      </ol>
+    </>
   )
 }
 
